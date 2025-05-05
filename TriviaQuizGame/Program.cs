9dc0cc1b4bdea24e997ba2ca30165600d593eff0
DevvKuba <jrfitnessinbox@gmail.com>
2025-05-05 12:55:20 +0100

    Add project files.

---
+++ TriviaQuizGame/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TriviaQuizGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var easyQuestions = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean";
+
+            //HttpClient client = new HttpClient();
+            //client.BaseAddress = new Uri(easyQuestions);
+            //Console.WriteLine(client.BaseAddress);
+
+            ApiHelper.InitialiseClient();
+        }
+    }
+}
---
+++ TriviaQuizGame/QuizProcessor.cs
@@ -0,0 +1,18 @@
+﻿namespace TriviaQuizGame
+{
+    public class QuizProcessor
+    {
+        // load easy, medium or hard quiz
+        public async Task LoadQuiz(string QuizDifficulty)
+        {
+            string url = $"https://opentdb.com/api.php?amount=10&category=18&difficulty={QuizDifficulty}&type=multiple";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+
+            }
+
+
+        }
+    }
+}
